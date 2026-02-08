6151f0b841336f078dd8848b46d7904605be7591
abbaskashim <abbaskashimsardhar@gmail.com>
2026-02-08 07:20:09 +0000

    Docker Mini Project

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Docker Streamlit App")
+
+st.title("Hello from Docker 🐳")
+st.write("This Streamlit app is running inside a Docker container.")
